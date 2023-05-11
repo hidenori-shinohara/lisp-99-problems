34c4c8155cf9bc9106a6d3ce2d1d3bc3bbb4cffc
Hidenori Shinohara <hidenori@stellar.org>
2023-05-11 13:44:12 -0400

    Up to problem 10

--- 99problems.lisp
+++ 99problems.lisp
@@ -47,3 +47,49 @@
 (defun reverse-ls (ls)
   (reverse-ls-helper ls nil))
 
+; problem 6
+
+(defun palindrome (ls)
+  (equal ls (reverse-ls ls)))
+
+; problem 7
+
+(defun my-flatten (ls)
+  (cond
+      ((equal ls nil) nil)
+      ((listp (car ls)) (append (my-flatten (car ls)) (my-flatten (cdr ls))))
+      (t (cons (car ls) (my-flatten (cdr ls))))
+      )
+  )
+
+
+; problem 8
+
+(defun compress (ls)
+  (cond
+    ((<= (length ls) 1)  ls)
+    ((equal (car ls) (car (cdr ls))) (compress (cdr ls)))
+    (t (cons (car ls) (compress (cdr ls))))
+    )
+  )
+
+
+; problem 9
+(defun pack-helper (ls acc)
+  (cond
+    ((equal ls nil) (list acc))
+    ((equal acc nil) (pack-helper (cdr ls) (list (car ls))))
+    ((equal (car ls) (car acc)) (pack-helper (cdr ls) (cons (car ls) acc)))
+    (t (cons acc (pack-helper ls nil)))
+    )
+  )
+
+(defun pack (ls)
+  (pack-helper ls nil))
+
+
+; problem 10
+
+(defun encode (ls)
+  (mapcar #'(lambda (x) (cons (length x) (car x))) (pack ls)))
+
